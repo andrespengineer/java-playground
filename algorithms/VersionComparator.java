b4bb1905db919f198251a4f559296ae37d755646
Andrés Sierra Pena <asierra@oriontek.do>
2024-03-31 21:25:31 +0100

    Updating version comparator

--- algorithms/VersionComparator.java
+++ algorithms/VersionComparator.java
@@ -9,26 +9,44 @@ public class VersionComparator implements Comparator<String> {
 
         int i = 0, j = 0;
 
-        Integer bias = null;
-
         while(i < partsA.length && j < partsB.length) {
             String a = partsA[i++];
             String b = partsB[j++];
 
             int result = comparePart(a, b);
 
-            if(result == 0) {
-                if(a.length() != b.length() && bias == null) {
-                    bias = Integer.compare(b.length(), a.length());
-                }
-            }
-            else {
+            if(result != 0) {
                 return result;
             }
         }
 
-        return bias == null ? (partsA.length - partsB.length) : bias;
+        if(checkLeftover(partsA, i)){
+            return 1;
+        }
+
+        if(checkLeftover(partsB, j)){
+            return -1;
+        }
+
+        return 0;
     }
+
+    private boolean checkLeftover(String[] parts, int index){
+        if(index >= parts.length){
+            return false;
+        }
+
+        while(index < parts.length){
+            String part = parts[index++];
+            for(int i = 0; i < part.length(); i++){
+                if(part.charAt(i) != '0'){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
     private int comparePart(String a, String b){
 
@@ -42,12 +60,17 @@ public class VersionComparator implements Comparator<String> {
         }
 
         while(i < a.length() && j < b.length()){
+
             if(a.charAt(i) < b.charAt(j)){
-                return -1;
+                return nextIsDigit(a, i) && nextIsPoint(b, j) ? 1 : -1;
             }
-            if(a.charAt(i++) > b.charAt(j++)){
-                return 1;
+
+            if(a.charAt(i) > b.charAt(j)){
+                return nextIsDigit(b, j) && nextIsPoint(a, i) ? -1 : 1;
             }
+
+            i++;
+            j++;
         }
 
         if(i == a.length() && j == b.length()){
@@ -63,5 +86,21 @@ public class VersionComparator implements Comparator<String> {
         }
 
         return 0;
+    }
+
+    private boolean nextIsDigit(String version, int index){
+        if(++index >= version.length()){
+            return false;
+        }
+
+        return Character.isDigit(version.charAt(index));
+    }
+
+    private boolean nextIsPoint(String version, int index){
+        if(++index >= version.length()){
+            return true;
+        }
+
+        return version.charAt(index) == '.';
     }
 }
